94bb7f5afc7e7820c2191d0a4b0e08217c07fc3c
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2026-01-31 10:43:59 +0100

    Change host to addr

--- connection.go
+++ connection.go
@@ -55,15 +55,12 @@ func WithLogger(logger func(format string, a ...any)) func(*Connection) {
 
 // Creates a new connection to a RCT device at the given address.
 // Must not be called concurrently.
-func NewConnection(ctx context.Context, host string, port string, opt ...func(*Connection)) (*Connection, error) {
+func NewConnection(ctx context.Context, addr string, opt ...func(*Connection)) (*Connection, error) {
 	conn := &Connection{
 		cache:  newCache(),
 		broker: internal.NewBroker[*Datagram](),
 	}
 
-	if port == "" {
-		port = "8899"
-	}
 	for _, o := range opt {
 		o(conn)
 	}
@@ -71,7 +68,7 @@ func NewConnection(ctx context.Context, host string, port string, opt ...func(*C
 	bufC := make(chan byte, 1024)
 	errC := make(chan error, 1)
 
-	go conn.receive(ctx, net.JoinHostPort(host, port), bufC, errC)
+	go conn.receive(ctx, addr, bufC, errC)
 
 	// wait up to SuccessTimeout for first non-error response, i.e. successful read
 	var lastErr error
